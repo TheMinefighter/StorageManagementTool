a3bf3310120c9281b52e2eff3d551284af0ddb46
Tobias Brohl <tobrohl@gmail.com>
2018-05-11 20:19:14 +0200

    fixed errors in the previous commit

--- StorageManagementCore/WPFGUI/MainWindow.ShellFolder.cs
+++ StorageManagementCore/WPFGUI/MainWindow.ShellFolder.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
@@ -17,25 +18,30 @@ namespace StorageManagementCore.WPFGUI {
 				: AdvancedUserShellFolder.AllUSF.Where(x => !x.Undefined).Select(x => x.LocalizedName);
 		}
 
-		private void MoveDependentShellFoldersCb_Unchecked(object sender, RoutedEventArgs e) {
-			e.Handled = !Confirmation.Confirm();
+		private void ChangeDependentShellFoldersCb_Unchecked(object sender, RoutedEventArgs e) {
+			ChangeDependentShellFoldersCb.IsChecked = !Confirmation.Confirm();
 		}
 
 		private void MoveExistingItemsCb_Unchecked(object sender, RoutedEventArgs e) {
-			e.Handled = !Confirmation.Confirm();
+			MoveExistingItemsCb.IsChecked= !Confirmation.Confirm();
 		}
 
 		private void ViewHiddenFoldersCb_Checked(object sender, RoutedEventArgs e) {
-			RefreshShellFolders();
+			if (Confirmation.Confirm()) {
+				RefreshShellFolders();
+			}
+			else {
+				ViewHiddenFoldersCb.IsChecked = false;
+			}
 		}
 
 
 		private void ApplyShellFolderLocationBtn_Click(object sender, RoutedEventArgs e) {
-			e.Handled = !Confirmation.Confirm();
+//TODO do
 		}
 
 		private void OpenCurrentShellFolderPathBtn_OnClick(object sender, RoutedEventArgs e) {
-			throw new NotImplementedException();
+			Backend.FileAndFolder.OpenFolder(new DirectoryInfo(CurrentShellFolderPathTb.Text));
 		}
 
 		private void ViewHiddenFoldersCb_Unchecked(object sender, RoutedEventArgs e) {
