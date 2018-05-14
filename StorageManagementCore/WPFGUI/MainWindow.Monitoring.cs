67f69ab5dde67c5159a7a016afda236e1e76c96d
Tobias Brohl <tobrohl@gmail.com>
2018-05-14 10:12:05 +0200

    .

---
+++ StorageManagementCore/WPFGUI/MainWindow.Monitoring.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace StorageManagementCore.WPFGUI {
+	public partial class MainWindow {
+		private void MonitoringTi_OnLoaded(object sender, RoutedEventArgs e) {
+			
+		}
+	}
+}
---
+++ StorageManagementCore/WPFGUI/MainWindow.Pagefiles.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace StorageManagementCore.WPFGUI {
+	public partial class MainWindow {
+		private void PagefilesTi_OnLoaded(object sender, RoutedEventArgs e) {
+			
+		}
+	}
+}
