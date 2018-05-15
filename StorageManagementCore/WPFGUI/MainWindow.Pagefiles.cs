d1aeb0cf4bdbaa47b17d4e4c14537b07ddef988a
Tobias Brohl <tobrohl@gmail.com>
2018-05-15 21:33:15 +0200

    .

--- StorageManagementCore/WPFGUI/MainWindow.Pagefiles.cs
+++ StorageManagementCore/WPFGUI/MainWindow.Pagefiles.cs
@@ -1,13 +1,20 @@
 ﻿using System.Linq;
 using System.Windows;
 
-namespace StorageManagementCore.WPFGUI {
-	public partial class MainWindow {
-		private void PagefilesTi_OnLoaded(object sender, RoutedEventArgs e)
-		{
-			NewSwapfileDriveCmb.ItemsSource =
-				Backend.Wrapper.getDrives().Select(Operation.OperatingMethods.GetDriveInfoDescription);
-			DriveForNewPageFileCmb.ItemsSource= Backend.Wrapper.getDrives().Select(Operation.OperatingMethods.GetDriveInfoDescription);
+namespace StorageManagementCore.WPFGUI
+{
+   public partial class MainWindow
+   {
+      private void PagefilesTi_OnLoaded(object sender, RoutedEventArgs e)
+      {
+         NewSwapfileDriveCmb.ItemsSource =
+            Backend.Wrapper.getDrives().Select(Operation.OperatingMethods.GetDriveInfoDescription);
+         DriveForNewPageFileCmb.ItemsSource = Backend.Wrapper.getDrives().Select(Operation.OperatingMethods.GetDriveInfoDescription);
       }
-	}
+
+	   private void PagefilesTi_OnSelected(object sender, RoutedEventArgs e)
+	   {
+		   throw new System.NotImplementedException();
+	   }
+   }
 }
