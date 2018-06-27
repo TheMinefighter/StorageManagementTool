f15d7e67ee8f3f747417091a4db0fb09e40a0d4c
Tobias Brohl <tobrohl@gmail.com>
2018-06-27 16:10:28 +0200

    Ill try a different one

--- StorageManagementCore/WPFGUI/MainViewModel.cs
+++ StorageManagementCore/WPFGUI/MainViewModel.cs
@@ -5,24 +5,25 @@ using JetBrains.Annotations;
 using StorageManagementCore.Configuration;
 
 namespace StorageManagementCore.WPFGUI {
-	public class MainViewModel :INotifyPropertyChanged {
+	public class MainViewModel : INotifyPropertyChanged {
 		private Pagefile _selectedPagefile;
 
 		public Pagefile SelectedPagefile {
 			get => _selectedPagefile;
 			set {
 				if (!ReferenceEquals(value, _selectedPagefile)) {
-					_selectedPagefile = value; 
+					_selectedPagefile = value;
 					OnPropertyChanged(nameof(SelectedPagefile));
 				}
-
-				}
+			}
 		}
 
 		public PagefileSysConfiguration PagefileConfiguration { get; set; }
 
 		public MainViewModel() {
-			PagefileConfiguration= Operation.PagefileManagement.GetCurrentPagefileConfiguration(out PagefileSysConfiguration tmp)?tmp: new PagefileSysConfiguration();
+			PagefileConfiguration = Operation.PagefileManagement.GetCurrentPagefileConfiguration(out PagefileSysConfiguration tmp)
+				? tmp
+				: new PagefileSysConfiguration();
 			SelectedPagefile = null;
 		}
 
