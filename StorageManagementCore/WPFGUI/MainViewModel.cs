8c085c6beb250fa0b28e37a3a1763fcbcb449b7b
Tobias Brohl <tobrohl@gmail.com>
2018-06-27 13:35:14 +0200

    Now with viewModels

---
+++ StorageManagementCore/WPFGUI/MainViewModel.cs
@@ -0,0 +1,7 @@
+﻿using StorageManagementCore.Configuration;
+
+namespace StorageManagementCore.WPFGUI {
+	public class MainViewModel {
+		public PagefileSysConfiguration PagefileConfiguration { get; set; }
+	}
+}
