9be2a8c01365fcc57a5724d57ce469883105686b
Tobias Brohl aka The_Minefighter <tobrohl@gmail.com>
2018-09-05 23:09:28 +0200

    Working for UI Impl

--- src/StorageManagementCore/Configuration/UpdateConfiguration.cs
+++ src/StorageManagementCore/Configuration/UpdateConfiguration.cs
@@ -1,22 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using StorageManagementCore.Backend;
+
 namespace StorageManagementCore.Configuration {
 	public class UpdateConfiguration {
+		private static ReadOnlyMap<UpdateMode, string> _updateModeByLocalizedName;
+
+		public static ReadOnlyMap<UpdateMode, string> UpdateModeByLocalizedName => _updateModeByLocalizedName ??
+			(_updateModeByLocalizedName =new ReadOnlyMap<UpdateMode, string>(Enum.GetValues(typeof(UpdateMode)).Cast<UpdateMode>()
+				.Select(x => new KeyValuePair<UpdateMode, string>(x, LocalizedModeName(x)))));
 
 		public static string LocalizedModeName(UpdateMode updateMode) {
 			switch (updateMode) {
-					
+				case UpdateMode.NoUpdates:
+					return WPFGUI.GlobalizationRessources.SettingsStrings.UpdateMode_NoUpdates;
+				case UpdateMode.DownloadAndInstallOnStartup:
+					return WPFGUI.GlobalizationRessources.SettingsStrings.UpdateMode_DownloadAndInstallOnStartup;
+				case UpdateMode.DownloadOnStartupInstallNext:
+					return WPFGUI.GlobalizationRessources.SettingsStrings.UpdateMode_DownloadOnStartupInstallNext;
+				case UpdateMode.DownloadOnStartupInstallManual:
+					return WPFGUI.GlobalizationRessources.SettingsStrings.UpdateMode_DownloadOnStartupInstallManual;
+				default:
+					throw new ArgumentOutOfRangeException(nameof(updateMode), updateMode, null);
 			}
 		}
-		
+
 		public bool UsePreReleases;
 		public UpdateMode Mode;
+
 		public UpdateConfiguration(bool usePreReleases, UpdateMode mode) {
 			UsePreReleases = usePreReleases;
 			Mode = mode;
 		}
 
-		public UpdateConfiguration() {
-			
-		}
+		public UpdateConfiguration() { }
+
+		static UpdateConfiguration() {
+			Session.LanguageChanged += (a, b) => _updateModeByLocalizedName = null; }
+
 		public static UpdateConfiguration Default => new UpdateConfiguration {
 			UsePreReleases = true,
 			Mode = UpdateMode.DownloadOnStartupInstallManual
---
+++ src/StorageManagementCore/WPFGUI/DataProviders/UpdateModesProvider.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+using StorageManagementCore.Configuration;
+
+namespace StorageManagementCore.WPFGUI.DataProviders {
+	public class UpdateModesProvider : INotifyPropertyChanged {
+		public ReadOnlyCollection<UpdateMode> UpdateModes { get; } = Enum.GetValues(typeof(UpdateMode)).Cast<UpdateMode>().ToList().AsReadOnly();
+
+		public UpdateModesProvider() => Session.LanguageChanged += (a, b) => OnPropertyChanged(nameof(UpdateModes));
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		[NotifyPropertyChangedInvocator]
+		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) {
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
